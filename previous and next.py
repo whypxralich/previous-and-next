92f35a864cd23082032090b474e85f2111b7d283
whypxralich <whypxrlch@gmail.com>
2023-10-23 21:52:47 +0300

    Create previous and next.py

---
+++ previous and next.py
@@ -0,0 +1,8 @@
+print ("Enter your number")
+number = int(input())
+print("Your number is:", number)
+
+previous = number - 1
+print ("Previous number for",number,"is:", previous)
+next = number + 1
+print ("Next number for",number,"is:", next)
